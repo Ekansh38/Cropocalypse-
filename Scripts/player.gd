a2345bc2823348881ef93a00e6a2ea5ccbad8af8
Ekansh Goenka <goenka.ekansh@gmail.com>
2025-07-25 16:04:15 +0800

    added charector and gun art

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,8 +4,10 @@ var vel: Vector2 = Vector2.ZERO
 var acc: Vector2 = Vector2.ZERO
 var dir: Vector2 = Vector2.ZERO
 signal shoot_fired(global_position: Vector2, direction: Vector2)
+
 var can_shoot: bool = true
 var shoot_cooldown: float = 0.6
+var last_move_key: String = "right" # Default to right
 
 @export var inv: Inv
 @export var friction := 8.0
@@ -17,8 +19,10 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("left"):
 		dir.x -= 1
+		last_move_key = "left"
 	if Input.is_action_pressed("right"):
 		dir.x += 1
+		last_move_key = "right"
 	if Input.is_action_pressed("up"):
 		dir.y -= 1
 	if Input.is_action_pressed("down"):
@@ -41,30 +45,49 @@ func _physics_process(delta: float) -> void:
 	velocity = vel
 	move_and_slide()
 
-	
 func _process(delta: float) -> void:
-	rotate_gun_to_mouse()
+	rotate_guns_to_mouse()
+	update_active_gun()
+
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		emit_shoot_signal()
 		can_shoot = false
 		await get_tree().create_timer(shoot_cooldown).timeout
 		can_shoot = true
-		
-func rotate_gun_to_mouse() -> void:
+
+func rotate_guns_to_mouse() -> void:
 	var mouse_pos = get_global_mouse_position()
-	var gun_node = $Sprite2D/Gun
-	var dir_to_mouse = (mouse_pos - gun_node.global_position).normalized()
-	gun_node.rotation = dir_to_mouse.angle()
+
+	var left_gun = $Sprite2D/LeftGun
+	var right_gun = $Sprite2D/RightGun
+
+	var left_dir = (mouse_pos - left_gun.global_position).angle()
+	var right_dir = (mouse_pos - right_gun.global_position).angle()
+
+	left_gun.rotation = left_dir + deg_to_rad(180)
+	right_gun.rotation = right_dir
+
+func update_active_gun():
+	var left = $Sprite2D/LeftGun
+	var right = $Sprite2D/RightGun
+	left.visible = last_move_key == "left"
+	right.visible = last_move_key == "right"
 
 func emit_shoot_signal():
-	var marker = $Sprite2D/Gun/Marker2D
+	var gun
+	if (last_move_key == "left"): 
+		gun = $Sprite2D/LeftGun 
+	else: 
+		gun = $Sprite2D/RightGun
+	var marker = gun.get_node("Marker2D")
 	var shoot_pos = marker.global_position
 	var mouse_pos = get_global_mouse_position()
 	var shoot_dir = (mouse_pos - shoot_pos).normalized()
-	
-	var spread = deg_to_rad(10)  
+
+	var spread = deg_to_rad(10)
 	var mis_aim_angle = randf_range(-spread, spread)
 	shoot_dir = shoot_dir.rotated(mis_aim_angle)
+
 	emit_signal("shoot_fired", shoot_pos, shoot_dir)
 
 func add_item(type):
@@ -72,5 +95,4 @@ func add_item(type):
 
 func take_damage():
 	Globals.player_health -= 5
-	
 	print(Globals.player_health)
